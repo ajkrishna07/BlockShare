c1e7c25ea728383ae812497049aecb0ca41a8350
Ajay Krishna <kiran.ajay07@gmail.com>
2019-02-20 11:33:12 +0530

    Separated receiving function from SendReceive to Receive

---
+++ app/src/main/java/com/ajay/blockshare/Receive.java
@@ -0,0 +1,162 @@
+package com.ajay.blockshare;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.view.View;
+import android.content.Intent;
+import android.Manifest;
+import android.widget.TextView;
+import android.widget.Toast;
+import android.content.pm.PackageManager;
+import androidx.annotation.CallSuper;
+import androidx.annotation.NonNull;
+import androidx.core.content.ContextCompat;
+import android.content.Context;
+import com.google.android.gms.nearby.Nearby;
+import com.google.android.gms.nearby.connection.AdvertisingOptions;
+import com.google.android.gms.nearby.connection.ConnectionInfo;
+import com.google.android.gms.nearby.connection.ConnectionLifecycleCallback;
+import com.google.android.gms.nearby.connection.ConnectionResolution;
+import com.google.android.gms.nearby.connection.ConnectionsClient;
+import com.google.android.gms.nearby.connection.DiscoveredEndpointInfo;
+import com.google.android.gms.nearby.connection.EndpointDiscoveryCallback;
+import com.google.android.gms.nearby.connection.Payload;
+import com.google.android.gms.nearby.connection.PayloadCallback;
+import com.google.android.gms.nearby.connection.PayloadTransferUpdate;
+import com.google.android.gms.nearby.connection.Strategy;
+
+public class Receive extends AppCompatActivity {
+    private static final String[] REQUIRED_PERMISSIONS =
+            new String[] {
+                    Manifest.permission.BLUETOOTH,
+                    Manifest.permission.BLUETOOTH_ADMIN,
+                    Manifest.permission.ACCESS_WIFI_STATE,
+                    Manifest.permission.CHANGE_WIFI_STATE,
+                    Manifest.permission.ACCESS_COARSE_LOCATION,
+            };
+    private static final int REQUEST_CODE_REQUIRED_PERMISSIONS = 1;
+    private static final Strategy STRATEGY = Strategy.P2P_STAR;
+    private ConnectionsClient connectionsClient;
+    private final String codeName = CodenameGenerator.generate();
+    private String opponentEndpointId;
+    private String opponentName;
+    Context context = this;
+    TextView statusMessage;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_receive);
+        statusMessage = findViewById(R.id.statmsg);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        connectionsClient = Nearby.getConnectionsClient(this);
+
+        if (!hasPermissions(this, REQUIRED_PERMISSIONS)) {
+            requestPermissions(REQUIRED_PERMISSIONS, REQUEST_CODE_REQUIRED_PERMISSIONS);
+        }
+
+        startAdvertising();
+    }
+
+    @Override
+    protected void onDestroy() {
+        connectionsClient.stopAllEndpoints();
+        super.onDestroy();
+    }
+
+    /** Returns true if the app was granted all the permissions. Otherwise, returns false. */
+    private static boolean hasPermissions(Context context, String... permissions) {
+        for (String permission : permissions) {
+            if (ContextCompat.checkSelfPermission(context, permission)
+                    != PackageManager.PERMISSION_GRANTED) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    /** Handles user acceptance (or denial) of our permission request. */
+    @CallSuper
+    @Override
+    public void onRequestPermissionsResult(
+            int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        if (requestCode != REQUEST_CODE_REQUIRED_PERMISSIONS) {
+            return;
+        }
+
+        for (int grantResult : grantResults) {
+            if (grantResult == PackageManager.PERMISSION_DENIED) {
+                Toast.makeText(this, R.string.error_missing_permissions, Toast.LENGTH_LONG).show();
+                finish();
+                return;
+            }
+        }
+        recreate();
+    }
+
+    private void startAdvertising() {
+        AdvertisingOptions advertisingOptions = new AdvertisingOptions.Builder().setStrategy(STRATEGY).build();
+        connectionsClient.startAdvertising(codeName, getPackageName(), connectionLifecycleCallback, advertisingOptions);
+    }
+
+    private final EndpointDiscoveryCallback endpointDiscoveryCallback =
+            new EndpointDiscoveryCallback() {
+                @Override
+                public void onEndpointFound(String endpointId, DiscoveredEndpointInfo info) {
+                    connectionsClient.requestConnection(codeName, endpointId, connectionLifecycleCallback);
+                }
+
+                @Override
+                public void onEndpointLost(String endpointId) {}
+            };
+
+    private final ConnectionLifecycleCallback connectionLifecycleCallback =
+            new ConnectionLifecycleCallback() {
+                @Override
+                public void onConnectionInitiated(String endpointId, ConnectionInfo connectionInfo) {
+                    connectionsClient.acceptConnection(endpointId, payloadCallback);
+                    opponentName = connectionInfo.getEndpointName();
+                }
+
+                @Override
+                public void onConnectionResult(String endpointId, ConnectionResolution result) {
+                    if (result.getStatus().isSuccess()) {
+                        connectionsClient.stopDiscovery();
+                        connectionsClient.stopAdvertising();
+                        opponentEndpointId = endpointId;
+                        statusMessage.setText("Connected to " + opponentEndpointId);
+                    }
+                }
+
+                @Override
+                public void onDisconnected(String endpointId) {
+                    // We've been disconnected from this endpoint. No more data can be
+                    // sent or received.
+                }
+            };
+
+    private final PayloadCallback payloadCallback =
+            new PayloadCallback() {
+                @Override
+                public void onPayloadReceived(String endpointId, Payload payload) {
+                    byte[] receivedBytes = payload.asBytes();
+                    Context context = getApplicationContext();
+                    String text = new String(receivedBytes);
+                    int duration = Toast.LENGTH_SHORT;
+                    Toast toast = Toast.makeText(context, text, duration);
+                    toast.show();
+                }
+
+                @Override
+                public void onPayloadTransferUpdate(String endpointId, PayloadTransferUpdate update) {
+                    //
+                }
+            };
+}
